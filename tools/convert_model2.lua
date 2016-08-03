df6f95058cceeb16c7826383c31768f990158849
Xiaolong Wang <xiaolonw@cs.cmu.edu>
2016-08-02 22:31:16 -0400

    test

--- tools/convert_model2.lua
+++ tools/convert_model2.lua
@@ -8,14 +8,14 @@ require 'pl'
 require 'paths'
 
 
-network = '/nfs.yoda/xiaolonw/torch_projects/models/train_3dnormal_rgb/bactch60/adversarial_27.net'
-savefile = '/nfs.yoda/xiaolonw/torch_projects/models/train_3dnormal_rgb/bactch60/Style_GAN_nofcn.net'
+network = '/nfs.yoda/xiaolonw/torch_projects/models/train_3dnormal_joint4/save/joint_Style_GAN.net'
+savefile = '/nfs.yoda/xiaolonw/torch_projects/models/train_3dnormal_joint4/save/joint_Style_GAN.net'
 
 tmp = torch.load(network)
 model_D = tmp.D
 model_G = tmp.G
--- model_FCN = tmp.FCN 
+model_FCN = tmp.FCN 
 
-torch.save(savefile, {D = model_D:clearState(), G = model_G:clearState()})
+torch.save(savefile, {D = model_D:clearState(), G = model_G:clearState(), FCN = model_FCN:clearState()})
 
 
